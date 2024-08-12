d1678a1e1e0096edd5c04f07dc663396af0c22c6
Özgür Deniz Türker <ozgurdenizturker@Ozgur-MacBook-Pro.local>
2024-08-12 16:57:21 +0200

    configure auth server with default configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
+
+	id("org.jmailen.kotlinter") version "4.4.1"
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
 }
